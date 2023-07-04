7053b65bcf754702ce51758a3d66dd09ddee6bb9
Tonio Loewald <tonio@loewald.com>
2023-07-04 22:47:09 +0300

    <data-table> Column Resizing
    [new] columns can be resized
    [wip] column sorting

--- src/data-table.ts
+++ src/data-table.ts
@@ -1,19 +1,26 @@
 import { Component as WebComponent, elements, xin, vars } from 'xinjs'
 
-function defaultWidth(array: any[], prop: string, charWidth: number): string | boolean {
+function defaultWidth(array: any[], prop: string, charWidth: number): number | boolean {
   const example = array.find(item => item[prop] !== undefined && item[prop] !== null)
   if (example !== undefined) {
-    switch(typeof example[prop]) {
+    const value = example[prop]
+    switch(typeof value) {
       case 'string':
-        return (12 * charWidth) + 'px'
+        if (value.match(/^\d+(\.\d+)?$/)) {
+          return (6 * charWidth)
+        } else if (value.includes(' ')) {
+          return (20 * charWidth)
+        } else {
+          return (12 * charWidth)
+        }
       case 'number':
-        return (6 * charWidth) + 'px'
+        return (6 * charWidth)
       case 'boolean':
-        return (5 * charWidth) + 'px'
+        return (5 * charWidth)
       case 'object':
         return false
       default:
-        return (8 * charWidth) + 'px'
+        return (8 * charWidth)
     }
   }
   return false
@@ -21,11 +28,40 @@ function defaultWidth(array: any[], prop: string, charWidth: number): string | b
 
 const {div, span, template} = elements
 
+type TrackerCallback = ((event: Event) => boolean | undefined) | ((event: Event) => void)
+
+const trackMouse = (callback: TrackerCallback, cursor: string): void => {
+  const tracker = div({
+    style: {
+      content: ' ',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      cursor
+    }
+  })
+  // @ts-expect-error
+  document.body.append(tracker)
+  tracker.addEventListener('mousemove', (event: Event) => {
+    if (callback(event) === true) {
+      tracker.remove()
+    }
+  })
+  tracker.addEventListener('mouseup', (event: Event) => {
+    callback(event)
+    tracker.remove()
+  })
+
+}
+
 interface ColumnOptions {
   name?: string
   prop: string
-  width?: string | number
+  width: number
   visible?: boolean
+  sortable?: boolean
 }
 
 class DataTable extends WebComponent {
@@ -33,6 +69,7 @@ class DataTable extends WebComponent {
   columns: ColumnOptions[] | undefined
   charWidth = 15
   rowHeight = 30
+  minColumnWidth = 30
 
   private data: {
     columns: ColumnOptions[]
@@ -43,9 +80,48 @@ class DataTable extends WebComponent {
 
   constructor() {
     super()
-    this. data = {
+    this.data = {
       columns: [],
       array: []
+    }
+  }
+
+  getColumn(event: Event): ColumnOptions | undefined {
+    // @ts-expect-error
+    const x = event.clientX - this.getBoundingClientRect().x
+    let boundaryX = 0
+    const log: any[] = []
+    const column = this.data.columns.find(options => {
+      if (options.visible !== false) {
+        boundaryX += options.width
+        log.push(boundaryX)
+        return Math.abs(x - boundaryX) < 5
+      }
+    })
+    return column
+  }
+
+  trackMouse = (event: Event) => {
+    const column = this.getColumn(event)
+    if (column !== undefined) {
+      this.style.cursor = 'col-resize'
+    } else {
+      this.style.cursor = ''
+    }
+  }
+
+  resizeColumn = (event: Event) => {
+    const column = this.getColumn(event)
+    if (column !== undefined) {
+      const origWidth = column.width
+      // @ts-expect-error
+      const origX = event.clientX
+      trackMouse((event: Event) => {
+        // @ts-expect-error
+        const width = event.clientX - origX + origWidth
+        column.width = width > this.minColumnWidth ? width : this.minColumnWidth
+        this.setColumnWidths()
+      }, 'col-resize')
     }
   }
 
@@ -56,6 +132,17 @@ class DataTable extends WebComponent {
     xin[this.instanceId] = this.data
     // @ts-expect-error
     this.data = xin[this.instanceId]
+
+    this.addEventListener('mousemove', this.trackMouse)
+    this.addEventListener('mousedown', this.resizeColumn)
+  }
+
+  get visibleColumns(): ColumnOptions[] {
+    return this.data.columns.filter(c => c.visible !== false)
+  }
+
+  setColumnWidths() {
+    this.style.setProperty('--grid-columns', this.visibleColumns.map(c => c.width + 'px').join(' '))
   }
 
   render() {
@@ -82,10 +169,10 @@ class DataTable extends WebComponent {
           } as ColumnOptions
         })
       }
-      const visibleColumns = data.columns.filter(c => c.visible !== false)
       this.style.display = 'flex'
       this.style.flexDirection = 'column'
-      this.style.setProperty('--grid-columns', visibleColumns.map(c => c.width).join(' '))
+      const {visibleColumns} = this
+      this.setColumnWidths()
       const rowStyle = {
         display: 'grid',
         gridTemplateColumns: vars.gridColumns,
@@ -96,6 +183,11 @@ class DataTable extends WebComponent {
         textOverflow: 'ellipsis',
         whiteSpace: 'nowrap'
       }
+      const sorterStyle = {
+        display: 'inline-block',
+        minWidth: vars.charWidth,
+        cursor: 'default'
+      }
       this.append(
         div(
           { class: 'thead' },
@@ -104,7 +196,11 @@ class DataTable extends WebComponent {
               class: 'tr',
               style: rowStyle,
             },
-            ...visibleColumns.map(c => span({class: 'th', style: cellStyle}, c.name))
+            ...visibleColumns.map(c => span(
+              {class: 'th', style: cellStyle},
+              c.name,
+              c.sortable !== false ? span({class: 't-sorter', style: sorterStyle}) : undefined
+            ))
           ),
         ),
         div(
