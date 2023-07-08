181eaccf7fc2c357fa370fad163ce1cdad42a1f6
Tonio Loewald <tonio@loewald.com>
2023-07-08 19:26:37 +0300

    [new] <filter-builder> improvements
    [new] made availableFilters into a plugin architecture
    [new] :< and :> are now < and >
    [new] <, > work numerically if appropriate
    [new] added =
    [new] placeholder is generated from filters
    [new] description is computed and inserted in title

--- src/filter-builder.ts
+++ src/filter-builder.ts
@@ -7,6 +7,138 @@ type ArrayFilter = (array: any[]) => any[]
 const passThru = (array: any[]) => array
 const NULL_FILTER_DESCRIPTION = 'null filter, everything matches'
 
+type ObjectTest = (obj: any) => boolean
+
+interface FilterMaker {
+  hint: string
+  description: (...matches: string[]) => string
+  token: RegExp
+  makeFilter: (...matches: string[]) => ObjectTest
+}
+
+export const availableFilters: FilterMaker[] = [
+  {
+    hint: 'field=value',
+    description: (field: string, value: string) =>
+      isNaN(Number(value))
+        ? field !== ''
+          ? `${field} is "${value}"`
+          : `any field is "${value}"`
+        : field !== ''
+        ? `${field} == ${value}`
+        : `any field == ${value}`,
+    token: /^([\w-_]*)=(.+)$/,
+    makeFilter: (field: string, value: string) => {
+      if (isNaN(Number(value))) {
+        value = String(value).toLocaleLowerCase()
+        if (field !== '') {
+          return (obj: any) => String(obj[field]).toLocaleLowerCase() == value
+        }
+        return (obj: any) =>
+          Object.values(obj).find(
+            (val) => String(val).toLocaleLowerCase() == value
+          ) !== undefined
+      }
+      const num = Number(value)
+      if (field !== '') {
+        return (obj: any) => Number(obj[field]) == num
+      }
+      return (obj: any) => Object.values(obj).find((val) => Number(val) == num)
+    },
+  },
+  {
+    hint: 'field!=value',
+    description: (field: string, value: string) => `${field} ≠ ${value}`,
+    token: /^([\w-_]+)!=(.+)$/,
+    makeFilter: (field: string, value: string) => {
+      value = value.toLocaleLowerCase()
+      return (obj: any) => String(obj[field]).toLocaleLowerCase() != value
+    },
+  },
+  {
+    hint: 'field>value',
+    description: (field: string, value: string) =>
+      isNaN(Number(value))
+        ? `${field} > ${value} (A-Z)`
+        : `${field} > ${value}`,
+    token: /^([\w-_]+)>(.+)$/,
+    makeFilter: (field: string, value: string) => {
+      if (!isNaN(Number(value))) {
+        const num = Number(value)
+        return (obj: any) => Number(obj[field]) > num
+      }
+      value = value.toLocaleLowerCase()
+      return (obj: any) => String(obj[field]).toLocaleLowerCase() > value
+    },
+  },
+  {
+    hint: 'field<value',
+    description: (field: string, value: string) =>
+      isNaN(Number(value))
+        ? `${field} < ${value} (A-Z)`
+        : `${field} < ${value}`,
+    token: /^([\w-_]+)<(.+)$/,
+    makeFilter: (field: string, value: string) => {
+      if (!isNaN(Number(value))) {
+        const num = Number(value)
+        return (obj: any) => Number(obj[field]) < num
+      }
+      value = value.toLocaleLowerCase()
+      return (obj: any) => String(obj[field]).toLocaleLowerCase() < value
+    },
+  },
+  {
+    hint: 'field:value',
+    description: (field: string, value: string) => `${field} contains ${value}`,
+    token: /^([\w-_]+):(.+)$/,
+    makeFilter: (field: string, value: string) => {
+      value = value.toLocaleLowerCase()
+      return (obj: any) => obj[field].toLocaleLowerCase().includes(value)
+    },
+  },
+  {
+    hint: '!!field',
+    description: (match: string) => `${match} is truthy`,
+    token: /^!!([\w-_]+)$/,
+    makeFilter: (match: string) => (obj: any) => !!obj[match],
+  },
+  {
+    hint: '!field',
+    description: (match: string) => `${match} is falsy`,
+    token: /^!([\w-_]+)$/,
+    makeFilter: (match: string) => (obj: any) => !obj[match],
+  },
+  {
+    hint: 'value',
+    description: (match: string) => `"${match}" in any property`,
+    token: /^(.+)$/,
+    makeFilter: (match: string) => (obj: any) =>
+      Object.values(obj).find((value) =>
+        String(value).toLocaleLowerCase().includes(match)
+      ) !== undefined,
+  },
+]
+
+interface Filter {
+  description: string
+  test: ObjectTest
+}
+
+export function getFilter(
+  term: string,
+  filters = availableFilters
+): Filter | undefined {
+  const maker = filters.find((filter: FilterMaker) => filter.token.test(term))
+  if (maker !== undefined) {
+    // we know it will match
+    const [, ...terms] = term.match(maker.token)!
+    return {
+      description: maker.description(...terms),
+      test: maker.makeFilter(...terms),
+    }
+  }
+}
+
 class FilterBuilder extends WebComponent {
   value = ''
   filter: ArrayFilter = passThru
@@ -16,81 +148,48 @@ class FilterBuilder extends WebComponent {
       width: '100%',
       height: '100%',
     },
-    placeholder: 'needle name:foo !isFalse isTrue! lat:>0',
   })
+  placeholder = ''
+
+  filters = availableFilters
 
   constructor() {
     super()
-    this.initAttributes('title')
+    this.initAttributes('title', 'placeholder')
   }
 
   buildFilter = debounce((query: string): void => {
-    const descriptions: string[] = []
-    const filters = query
+    // @ts-expect-error
+    const filters: Filter[] = query
       .split(/\s+/)
       .filter((part) => part !== '')
-      .map((part) => {
-        if (part.startsWith('!')) {
-          const haystack = part.slice(1)
-          descriptions.push(`"${haystack}" is truthy`)
-          return (obj: any) => !obj[haystack]
-        } else if (part.endsWith('!')) {
-          const haystack = part.slice(0, part.length - 1)
-          descriptions.push(`"${haystack}" is falsy`)
-          return (obj: any) => !!obj[haystack]
-        } else if (part.includes(':')) {
-          let [haystack, needle] = part.split(':')
-          if (needle.startsWith('<')) {
-            descriptions.push(`"${needle}" < "${haystack}"`)
-            needle = needle.slice(1)
-            return (obj: any) => obj[haystack] && obj[haystack] < needle
-          } else if (needle.startsWith('>')) {
-            needle = needle.slice(1)
-            descriptions.push(`"${needle}" > "${haystack}"`)
-            return (obj: any) => obj[haystack] && obj[haystack] > needle
-          } else {
-            needle = needle.toLocaleLowerCase()
-            descriptions.push(`"${needle}" in "${haystack}"`)
-            return (obj: any) => {
-              return (
-                obj[haystack] &&
-                String(obj[haystack]).toLocaleLowerCase().includes(needle)
-              )
-            }
-          }
-        } else {
-          const needle = part.toLocaleLowerCase()
-          descriptions.push(`"${needle}" anywhere`)
-          return (obj: any) =>
-            Object.keys(obj).find((key) => {
-              return String(obj[key]).toLocaleLowerCase().includes(needle)
-            })
-        }
-      })
+      .map((part) => getFilter(part, this.filters))
+      .filter((part) => part !== undefined) // because we remove undefined
 
-    let filter: ArrayFilter
-    let title: string
     if (filters.length === 0) {
-      title = NULL_FILTER_DESCRIPTION
-      filter = passThru
+      this.title = NULL_FILTER_DESCRIPTION
+      if (this.filter !== passThru) {
+        this.filter = passThru
+        this.dispatchEvent(new Event('change'))
+      }
     } else {
-      title = descriptions.join(', and ')
-      filter = (array: any[]): any[] =>
-        array.filter((obj) => {
-          if (obj === null || typeof obj !== 'object') {
-            return false
+      this.filter = filters.reduce(
+        (f: false | ArrayFilter, filter: Filter): ArrayFilter => {
+          let g: ArrayFilter
+          if (f === false) {
+            g = (array: any[]) => array.filter(filter.test)
           } else {
-            return filters.find((filter) => !filter(obj)) === undefined
+            g = (array: any[]) => f(array.filter(filter.test))
           }
-        })
-    }
+          return g
+        },
+        false
+      ) as ArrayFilter // because this array is not empty
 
-    if (this.filter !== filter) {
-      this.title = title
-      this.filter = filter
+      this.title = filters.map((f) => f.description).join(', and ')
       this.dispatchEvent(new Event('change'))
     }
-  })
+  }, 500)
 
   reset() {
     if (this.filter !== passThru) {
@@ -110,6 +209,7 @@ class FilterBuilder extends WebComponent {
 
   connectedCallback(): void {
     super.connectedCallback()
+    this.setAttribute('title', this.title)
 
     const { input } = this.refs
     input.value = this.value
@@ -121,6 +221,10 @@ class FilterBuilder extends WebComponent {
   render() {
     super.render()
     const { input } = this.refs
+    input.placeholder =
+      this.placeholder !== ''
+        ? this.placeholder
+        : this.filters.map((filter) => filter.hint).join(' ')
     input.value = this.value
   }
 }
