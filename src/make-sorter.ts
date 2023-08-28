e688ae62310c1256f626b091087c3cb0bdab6635
Tonio Loewald <tonio@loewald.com>
2023-08-28 11:07:50 -0700

    [new] interactive example for makeSorter

--- src/make-sorter.ts
+++ src/make-sorter.ts
@@ -12,61 +12,85 @@ other than an array of numbers or strings easier.
 Usage:
 
 ```
-interface Person {
-  nameFirst: string
-  nameLast: string
+// creates a sort function that sorts by propA, then propB (if propA is tied)
+const sorter = makeSorter(obj => obj.propA, obj.propB)
+// as above but descending
+const sorter = makeSorter(obj => obj.propA, obj.propB, false)
+// as above but propA is sorted ascending, propB descending
+const sorter = makeSorter(obj => obj.propA, obj.propB, [false, true])
+```
+
+Examples:
+
+```css
+.preview {
+  padding: var(--spacing);
+}
+```
+```js
+const { select, option, div, ul, li } = xinjs.elements
+const { makeSorter } = xinjsui
+
+const people = [
+  { first: 'Juanita', last: 'Citizen', age: 25 },
+  { first: 'Rebecca', last: 'Howe', age: 35 },
+  { first: 'Jane', last: 'Doe', age: 35 },
+  { first: 'Sam', last: 'Malone', age: 40 },
+  { first: 'Norm', last: 'Peterson', age: 38 },
+]
+
+const sorters = {
+  firstSort: makeSorter(person => [person.first]),
+  firstDescSort: makeSorter(person => [person.first], false),
+  nameSort: makeSorter(person => [person.last, person.first]),
+  ageFirst: makeSorter(person => [-person.age, person.last]),
+  ageLast: makeSorter(person => [person.age, person.first], [true, false]),
 }
 
-const people: Person[] = await getRecords(...)
-
-// sort by nameLast, then nameFirst
-const nameSorter = makeSorter(
-  (person: Person) => [person.nameLast, person.nameFirst]
-)
-
-people.sort(nameSorter)
-```
-
-Here's a slightly more complex example:
-
-```
-interface Product {
-  name: string,
-  category: string,
-  sales: number
+function person({first, last, age}) {
+  return li(`${first} ${last}, ${age}`)
 }
 
-const productSales = await getRecords(...)
-
-// sort by category (A-Z), then sales (highest to lowest)
-const categorySalesSorter = makeSorter(
-  (product: Product) => [product.category, -product.sales]
+const list = ul()
+sortPicker = select(
+  option('Sort by first', {value: 'firstSort'}),
+  option('Sort by first (desc)', {value: 'firstDescSort'}),
+  option('Sort by last, first', {value: 'nameSort'}),
+  option('Sort by age (desc), first', {value: 'ageFirst'}),
+  option('Sort by age, last (desc)', {value: 'ageLast'}),
+  {
+    onChange: render,
+    value: 'nameSort'
+  },
 )
 
-productSales.sort(categorySalesSorter)
+function render () {
+  list.textContent = ''
+  list.append(...people.sort(sorters[sortPicker.value]).map(person))
+}
+
+preview.append(
+  sortPicker,
+  list
+)
+
+render()
 ```
-
-Basically, you write a function that given some thing returns a prioritized list of
-`string`s or `number`s and `makeSorter` produces an callback function for `Array.sort()`
-that will sort the array in _ascending_ order.
-
-If you pass `false` as the (optional) second parameter you'll get a _descending_ sorter,
-but for numbers just multiplying by -1 is just as easy (per the example).
-
-If I ever conceive of a need for a version that lets you invert the sort order of
-multiple non-numerical array elements I'll extend it.
 */
 
 export function makeSorter<T>(
   sortValuator: SortValuator<T>,
-  ascending = true
+  ascending: boolean | boolean[] = true
 ): SortCallback<T> {
   return (p: any, q: any) => {
     const pSort = sortValuator(p)
     const qSort = sortValuator(q)
     for (const i in pSort) {
       if (pSort[i] !== qSort[i]) {
-        return ascending
+        const isAscending = Array.isArray(ascending)
+          ? ascending[i] !== false
+          : ascending
+        return isAscending
           ? pSort[i] > qSort[i]
             ? 1
             : -1
