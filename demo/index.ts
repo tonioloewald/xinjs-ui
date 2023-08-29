e941f2f96e45051f093dcb13e3e1c419d9e61862
Tonio Loewald <tonio@loewald.com>
2023-08-29 07:15:45 -0700

    [new] <data-table> has a live demo [new] <filter-builder> is now graphical and has a live demo

--- demo/index.ts
+++ demo/index.ts
@@ -9,17 +9,11 @@ import {
 } from 'xinjs'
 
 import {
-  tabSelector,
   markdownViewer,
-  dataTable,
-  filterBuilder,
-  FilterBuilder,
   liveExample,
-  scriptTag,
   sideNav,
   SideNav,
   sizeBreak,
-  TableData,
 } from '../src'
 
 import docs from './docs.json'
@@ -31,9 +25,6 @@ console.log(
   )}; padding: 0 5px;`
 )
 
-const deathsUrl =
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
-
 const PROJECT = 'xinjs-ui'
 
 const { app } = xinProxy({
@@ -44,13 +35,6 @@ const { app } = xinProxy({
     xinjsUrl: 'https://xinjs.net',
     bundleUrl: `https://deno.bundlejs.com/?q=${PROJECT}&badge=`,
     optimizeLottie: false,
-    tableData: {
-      columns: null as null | any[],
-      array: [
-        { id: 'NCC-1701', name: 'Enterprise', number: 17, hasKirk: true },
-        { id: 'NCC-74656-A', name: 'Voyager', number: 74, hasKirk: false },
-      ] as any[],
-    } as TableData,
     lottieFilename: '',
     lottieData: '',
     currentDoc:
@@ -60,36 +44,6 @@ const { app } = xinProxy({
     docs,
   },
 })
-
-bindings.columns = {
-  toDOM(elt: any, columns) {
-    elt.columns = columns
-  },
-}
-;(async () => {
-  const { Papa } = await scriptTag(
-    'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js'
-  )
-
-  const response = await fetch(deathsUrl)
-  const csv = await response.text()
-  const rows = Papa.parse(csv).data.map((row: string) => [
-    ...row.slice(0, 4),
-    ...row.slice(-5),
-  ])
-  const columns = rows.shift()
-
-  app.tableData = {
-    columns: null,
-    array: rows.map((row: string[]) =>
-      row.reduce((obj, value, idx) => {
-        obj[columns[idx]] = value
-        return obj
-      }, {} as { [key: string]: any })
-    ),
-    filter: undefined,
-  }
-})()
 
 bindings.docLink = {
   toDOM(elt, filename) {
@@ -108,25 +62,7 @@ Object.assign(globalThis, { app, xin, bindings, elements, vars, touch })
 
 const main = document.querySelector('main') as HTMLElement
 
-const { h1, h2, div, span, a, img, label, input, header, button, template } =
-  elements
-
-const table = dataTable({
-  style: {
-    flex: '1 1 auto',
-  },
-  rowHeight: 34,
-  bindValue: 'app.tableData',
-})
-
-const filter = filterBuilder({
-  class: 'elastic',
-  placeholder: 'enter query',
-  onChange(event: Event) {
-    app.tableData.filter = (event.target as FilterBuilder).filter
-    touch('app.tableData')
-  },
-})
+const { h1, div, span, a, img, header, button, template } = elements
 
 const docViewer = markdownViewer({
   style: {
@@ -196,264 +132,76 @@ main.append(
       href: app.npmUrl,
     })
   ),
-  tabSelector(
+  sideNav(
     {
+      name: 'Documentation',
+      navSize: 200,
+      minSize: 600,
       style: {
-        position: 'relative',
-        flex: '1 1 auto',
+        height: '100%',
       },
     },
-    sideNav(
+    div(
       {
-        name: 'Documentation',
-        navSize: 200,
-        minSize: 600,
+        slot: 'nav',
         style: {
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
           height: '100%',
+          background: vars.navBg,
+          overflowY: 'scroll',
+        },
+        bindList: {
+          value: app.docs,
         },
       },
-      div(
-        {
-          slot: 'nav',
-          style: {
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            height: '100%',
-            background: vars.navBg,
-            overflowY: 'scroll',
+      template(
+        a({
+          class: 'doc-link',
+          bindText: '^.filename',
+          bindCurrent: 'app.currentDoc',
+          bindDocLink: '^.filename',
+          onClick(event: Event) {
+            const a = event.target as HTMLAnchorElement
+            const doc = getListItem(event.target as HTMLElement)
+            const nav = (event.target as HTMLElement).closest(
+              'side-nav'
+            ) as SideNav
+            nav.contentVisible = true
+            const { href } = a
+            window.history.pushState({ href }, '', href)
+            app.currentDoc = doc.filename
+            docViewer.value = doc.text
+            event.preventDefault()
           },
-          bindList: {
-            value: app.docs,
-          },
-        },
-        template(
-          a({
-            class: 'doc-link',
-            bindText: '^.filename',
-            bindCurrent: 'app.currentDoc',
-            bindDocLink: '^.filename',
-            onClick(event: Event) {
-              const a = event.target as HTMLAnchorElement
-              const doc = getListItem(event.target as HTMLElement)
-              const nav = (event.target as HTMLElement).closest(
-                'side-nav'
-              ) as SideNav
-              nav.contentVisible = true
-              const { href } = a
-              window.history.pushState({ href }, '', href)
-              app.currentDoc = doc.filename
-              docViewer.value = doc.text
-              event.preventDefault()
-            },
-          })
-        )
-      ),
-      div(
-        {
-          style: {
-            position: 'relative',
-            overflowY: 'scroll',
-            height: '100%',
-          },
-        },
-        button(
-          {
-            class: 'transparent close-nav show-within-compact',
-            style: {
-              marginTop: '2px',
-              position: 'fixed',
-            },
-            onClick(event: Event) {
-              ;(
-                (event.target as HTMLElement).closest('side-nav') as SideNav
-              ).contentVisible = false
-            },
-          },
-          span({ class: 'icon-chevron-left' })
-        ),
-        docViewer
+        })
       )
     ),
     div(
       {
-        name: 'data table & filter builder',
         style: {
-          display: 'flex',
-          flexDirection: 'column',
-          overflow: 'hidden',
+          position: 'relative',
+          overflowY: 'scroll',
           height: '100%',
         },
       },
-      h2('data-table and filter-builder', { style: { textAlign: 'center' } }),
-      div(
+      button(
         {
-          class: 'bar',
+          class: 'transparent close-nav show-within-compact',
+          style: {
+            marginTop: '2px',
+            position: 'fixed',
+          },
+          onClick(event: Event) {
+            ;(
+              (event.target as HTMLElement).closest('side-nav') as SideNav
+            ).contentVisible = false
+          },
         },
-        label(
-          { class: 'clickable', tabindex: 0 },
-          'load json',
-          input({
-            type: 'file',
-            hidden: true,
-            accept: '.json,application/json',
-            onChange(event: Event) {
-              const { files } = event.target as HTMLInputElement
-              if (files && files.length === 1) {
-                const reader = new FileReader()
-                reader.onload = () => {
-                  const { result } = reader
-                  if (typeof result !== 'string') {
-                    return
-                  }
-                  app.tableData = {
-                    columns: null,
-                    array: JSON.parse(result),
-                  }
-                }
-                reader.readAsText(files[0])
-              }
-            },
-          })
-        ),
-        label(
-          { class: 'clickable', tabindex: 0 },
-          'load tsv',
-          input({
-            type: 'file',
-            hidden: true,
-            accept: '.tsv,text/tab-separated-values',
-            onChange(event: Event) {
-              const { files } = event.target as HTMLInputElement
-              if (files && files.length === 1) {
-                const reader = new FileReader()
-                reader.onload = () => {
-                  const { result } = reader
-                  if (typeof result !== 'string') {
-                    return
-                  }
-                  const rows = result.split('\n').map((row) => row.split('\t'))
-                  const keys = rows.shift() as string[]
-                  const array = rows.map((row) => {
-                    const obj: { [key: string]: string } = {}
-                    for (const i in keys) {
-                      obj[keys[i]] = row[i]
-                    }
-                    return obj
-                  })
-                  app.tableData = {
-                    columns: null,
-                    array,
-                  }
-                }
-                reader.readAsText(files[0])
-              }
-            },
-          })
-        ),
-        button('Filter Test Data', {
-          onClick() {
-            const standards = [
-              'calendar',
-              'collation',
-              'currency',
-              'numberingSystem',
-              'timeZone',
-              'unit',
-            ]
-            const things = standards
-
-              .map((standard: string) => {
-                // @ts-expect-error it is a legal value
-                const values = Intl.supportedValuesOf(standard)
-                return values.map((value) => ({ standard, value }))
-              })
-              .flat()
-            const array = things.map(
-              (
-                { standard, value }: { standard: string; value: string },
-                id
-              ) => ({
-                id: String(id),
-                standard,
-                name: value,
-                number: (Math.random() * 1e2 - 50).toFixed(2),
-                isLucky: Math.random() < 0.1,
-                date: new Date(
-                  Date.now() - Math.random() * 10 * 365 * 24 * 3600 * 1000
-                ).toLocaleString(),
-              })
-            )
-
-            filter.reset()
-            const columns = [
-              {
-                prop: 'id',
-                width: 60,
-                align: 'right',
-              },
-              {
-                prop: 'standard',
-                width: 140,
-              },
-              {
-                prop: 'name',
-                width: 250,
-              },
-              {
-                prop: 'number',
-                width: 100,
-                align: 'right',
-              },
-              {
-                prop: 'isLucky',
-                width: 80,
-                dataCell() {
-                  return span(
-                    {
-                      style: {
-                        textAlign: 'center',
-                      },
-                    },
-                    input({
-                      type: 'checkbox',
-                      bindValue: '^.isLucky',
-                      title: 'special',
-                    })
-                  )
-                },
-              },
-              {
-                prop: 'date',
-                width: 200,
-              },
-            ]
-            app.tableData = {
-              array,
-              columns,
-            }
-          },
-        }),
-        filter,
-        button(
-          {
-            class: 'iconic',
-            style: {
-              marginLeft: vars.spacing_50,
-            },
-            onClick(event: Event) {
-              ;(event.target as HTMLElement)
-                .closest('button')
-                ?.classList.toggle('on')
-              filter.classList.toggle('show-help')
-            },
-          },
-          span({
-            class: 'icon-help',
-          })
-        )
+        span({ class: 'icon-chevron-left' })
       ),
-      table
+      docViewer
     )
   )
 )
