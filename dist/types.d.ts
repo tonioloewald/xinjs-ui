601495edd3729778f6bb03e575a29a85f062e9ec
Tonio Loewald <tonio@loewald.com>
2023-09-04 11:36:27 -0700

    [tweak] movied bodymovin demo controls [tweak] alpha sorting icons

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -80,8 +80,8 @@ preview.append(
 
 .preview .panel {
   position: absolute;
-  right: 10px;
-  top: 10px;
+  left: 10px;
+  bottom: 10px;
   padding: 10px;
   border-radius: 5px;
   background: white;
