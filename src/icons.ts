af86f7f02a4bd49f513380e5c84e10d0247e4046
Tonio Loewald <tonio@loewald.com>
2023-09-06 11:13:15 -0700

    [fix] improved docs for icons [fix] nicer styling on live-example

--- src/icons.ts
+++ src/icons.ts
@@ -8,7 +8,7 @@ These icons are mainly sourced from [feather](https://github.com/feathericons/fe
 
 `icons` is simply a proxy that generates an `ElementCreator` for a given icon on demand,
 e.g. `icons.chevronDown()` produces an `<svg>` element containing a downward-pointing chevron
-icon.
+icon with the class `icon-chevron-down`.
 
 ```js
 const { icons } = xinjsui
@@ -29,8 +29,9 @@ preview.append(...Object.keys(icons).sort().map(iconName => div(
   overflow: hidden scroll !important;
 }
 
-.preview [class^="icon-"] {
+.preview svg {
   fill: var(--text-color);
+  height: 48px;
 }
 
 .preview .tile {
@@ -38,6 +39,9 @@ preview.append(...Object.keys(icons).sort().map(iconName => div(
   width: 160px;
   text-align: center;
   cursor: pointer;
+  background: #fff8;
+  padding: 10px;
+  border-radius: 5px;
 }
 
 .preview .tile:hover {
@@ -45,14 +49,14 @@ preview.append(...Object.keys(icons).sort().map(iconName => div(
 }
 
 .preview .tile > div {
+  font-family: Menlo, Monaco, monospace;
   whitespace: no-wrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  font-size: 80%;
+  font-size: 14px;
   line-height: 1.5;
 }
 ```
-
 
 */
 
