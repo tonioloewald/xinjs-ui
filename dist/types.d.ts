3d3e040964016e840bed09de0a1006ce61b645a9
Tonio Loewald <tonio@loewald.com>
2023-09-27 12:00:38 +0300

    [fix] made icon-data more efficient for common case

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -491,14 +491,14 @@ sizer.addEventListener('touchstart', resize, 'nwse-resize')
   border: 1px solid #aaa;
 }
 
-size-break * {
+.preview size-break * {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
 }
 
-.container {
+.preview .container {
   position: relative;
   min-width: 100px;
   min-height: 40px;
@@ -507,7 +507,7 @@ size-break * {
   height: 100px;
 }
 
-.sizer {
+.preview .sizer {
   position: absolute;
   width: 20px;
   height: 20px;
