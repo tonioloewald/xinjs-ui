afa4a31ee982aece77a41a93a6d177b4ceb48411
Tonio Loewald <tonio@loewald.com>
2023-11-24 19:33:28 +0200

    [new] switched localhost to https [new] added AR to babylon component [new] added styling for VR helper button

--- src/babylon-3d.ts
+++ src/babylon-3d.ts
@@ -5,24 +5,33 @@ A [babylonjs](https://www.babylonjs.com/) wrapper.
 
 A `<xin-3d>` element is initialized with an `engine`, `canvas`, `scene`, and an update-loop.
 
-
+If you view this example with a VR-enabled device, such as the
+[Meta Quest 3](https://www.meta.com/quest/quest-3/), then you should be able to view this
+as an AR scene.
 
 ```js
 const { b3d } = xinjsui
 
 preview.append(b3d({
   sceneCreated(element, BABYLON) {
-    const camera = new BABYLON.ArcRotateCamera(
+    const camera = new BABYLON.FreeCamera(
       'camera',
-      -Math.PI / 2,
-      Math.PI / 2.5,
-      3,
-      new BABYLON.Vector3(0, 0, 0)
+      new BABYLON.Vector3(0, 2, -2),
+      element.scene
     )
     camera.attachControl(element.parts.canvas, true)
 
     new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0.25, 1, 0.75))
-    BABYLON.MeshBuilder.CreateBox('box', {})
+    const box = BABYLON.MeshBuilder.CreateBox('box', {})
+    box.position.y = 1.25
+
+    if (navigator.xr) {
+      element.scene.createDefaultXRExperienceAsync({
+        uiOptions: {
+          sessionMode: 'immersive-ar'
+        }
+      })
+    }
   },
   update(element) {
     element.scene.getMeshByName('box').rotation.y += 0.005
@@ -69,6 +78,24 @@ export class B3d extends WebComponent {
     ':host canvas': {
       width: '100%',
       height: '100%',
+    },
+    ':host .babylonVRicon': {
+      height: 50,
+      width: 80,
+      backgroundColor: '#0004',
+      backgroundImage:
+        'url(data:image/svg+xml;charset=UTF-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%3Csvg%20version%3D%221.1%22%20viewBox%3D%220%200%20800%20500%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cg%20fill%3D%22none%22%20%3E%3Cpath%20d%3D%22m400%2069c157%200%20335%207%20335%2081v101c0%2041-42%20180-191%20180-43%200-64-17-82-33l-2-2-4-4c-15-13-30-25-55-25-24%200-39%2011-54%2024l-2%202-3%203-2%202c-18%2016-40%2033-82%2033-148%200-191-137-191-179l-0-1v-101c0-74%20178-81%20335-81z%22%20stroke%3D%22%23fffc%22%20stroke-width%3D%2240%22/%3E%3C/g%3E%3C/svg%3E)',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '80%',
+      border: 'none',
+      borderRadius: 5,
+      borderStyle: 'none',
+      outline: 'none',
+      transition: 'transform 0.125s ease-out',
+    },
+    ':host .babylonVRicon:hover': {
+      transform: 'scale(1.05)',
     },
   })
 
