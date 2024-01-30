6c5b7189aa33c2f53353e807af960a7a96fea891
Tonio Loewald <tonio@loewald.com>
2024-01-30 15:26:25 +0200

    [new] docs.js can now extract metadata and supports pinning [new] misc doc tweaks

--- docs.js
+++ docs.js
@@ -1,18 +1,63 @@
-// this code was originally written by chatGPT
-// then formatted by prettier
+/*!
+# notes on docs
+
+The `xinjs-ui` package includes `docs.js` which is used to build the documentation
+for the [ui.xinjs.net](https://ui.xinjs.net).
+
+This is a simple utility for finding all the markdown files in a directory and also all
+multi-line comments in .ts, .js, and .css source files that being with a "!".
+
+These comments are assumed to be in markdown.
+
+It then emits JSON containing all the content.
+
+Comments comprising JSON objects are treated as metadata and added to the
+file objects in the JSON data. This includes: `<!--{ ... }-- >` and `/*{...}* /`
+comments (omit the spaces inserted to prevent this text from blowing up docs.js!)
+
+As of now, the only metadata supported by docs.js is `pin` which if set to "top"
+will force the item to the top of the list, while "bottom" will force it to the
+bottom.
+
+This doc is pinned to the bottom. README is pinned to the top.
+
+> **Aside**: the original version of this code was written by ChatGPT.
+*/
+/*{"pin": "bottom"}*/
 
 const fs = require('fs')
 const path = require('path')
 
 const TRIM_REGEX = /^#+ |`/g
 
+function metadata(content, filePath) {
+  let source = content.match(/<\!\-\-(\{.*\})\-\->|\/*(\{.*\})\*\//)
+  let data = {}
+  if (source) {
+    try {
+      data = JSON.parse(source[1] || source[2])
+    } catch (e) {
+      console.error('bad metadata in doc', filePath)
+    }
+  }
+  return data
+}
+
+function pinnedSort(a, b) {
+  a =
+    (a.pin === 'top' ? 'A' : a.pin === 'bottom' ? 'Z' : 'M') +
+    a.title.toLocaleLowerCase()
+  b =
+    (b.pin === 'top' ? 'A' : b.pin === 'bottom' ? 'Z' : 'M') +
+    b.title.toLocaleLowerCase()
+  return a > b ? 1 : b > a ? -1 : 0
+}
+
 function findMarkdownFiles(dirs, ignore) {
   let markdownFiles = []
 
   function traverseDirectory(dir, ignore) {
-    const files = fs
-      .readdirSync(dir)
-      .sort((a, b) => (a.toLocaleLowerCase() < b.toLocaleLowerCase() ? -1 : 1))
+    const files = fs.readdirSync(dir)
     if (ignore.includes(dir)) {
       return
     }
@@ -30,6 +75,7 @@ function findMarkdownFiles(dirs, ignore) {
           title: content.split('\n')[0].replace(TRIM_REGEX, ''),
           filename: file,
           path: filePath,
+          ...metadata(content, filePath),
         })
       } else if (['.ts', '.js', '.css'].includes(path.extname(file))) {
         const content = fs.readFileSync(filePath, 'utf8')
@@ -43,6 +89,7 @@ function findMarkdownFiles(dirs, ignore) {
             title: text.split('\n')[0].replace(TRIM_REGEX, ''),
             filename: file,
             path: filePath,
+            ...metadata(content, filePath),
           })
         }
       }
@@ -53,7 +100,7 @@ function findMarkdownFiles(dirs, ignore) {
     traverseDirectory(dir, ignore)
   })
 
-  return markdownFiles
+  return markdownFiles.sort(pinnedSort)
 }
 
 function saveAsJSON(data, outputFilePath) {
