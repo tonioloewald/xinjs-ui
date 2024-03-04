3918597be161817e7891e6c77e08b7dbcee914ca
Tonio Loewald <tonio@loewald.com>
2024-03-04 17:52:18 +0200

    v0.6.20

--- dist/icons.d.ts
+++ dist/icons.d.ts
@@ -2,7 +2,8 @@
 # icons
 
 A library that provides `ElementCreator` functions that produce SVG icons. It leverages `xinjs`'s
-`svgElements` proxy.
+`svgElements` proxy and is intended to address all the key use-cases for SVG icons in web
+applications along with being very easy to extend and maintain.
 
 ## icons
 
@@ -16,7 +17,7 @@ const { div } = xinjs.elements
 
 preview.append(...Object.keys(icons).sort().map(iconName => div(
   { class: 'tile' },
-  svgIcon({icon: iconName}),
+  svgIcon({icon: iconName, size: 16}),
   div(iconName)
 )))
 ```
@@ -66,24 +67,45 @@ probably be broken out as a standalone library to allow the use of whatever icon
 
 `defineIcon(name: string, icon: IconSpec | string)` adds or replaces your own icons
 
-he simplest option is simply to pass the `path` attribute (if the icon has a single path) while more c
-omplex icons can be provide an `IconSpec` structure `{ p: string[]; w: number; h: number }` (specifying
+The simplest option is simply to pass the `path` attribute (if the icon has a single path) while more
+complex icons can be provide an `IconSpec` structure `{ p: string[]; w: number; h: number }` (specifying
 any number of paths and the size of the bounding box).
 
-Clearly it would be easy to extend this to allow multi-colored icons in the future.
+> This will be extended to allow multi-colored icons (whose colors can still be overridden via CSS) in the future.
 
 ## `<xin-icon>`
 
 `<xin-icon>` is a simple component that lets you embed icons as HTML. Check the CSS tab to see
 how it's styled.
 
-`<xin-icon>` supports two attributes:
+`<xin-icon>` supports four attributes:
 
+- `size` (defaults to 32) is the height of the icon in pixels
 - `icon` is the name of the icon
 - `color` is the fill color (if you don't want to style it using CSS)
+- `stroke` is the stroke color
+- `stroke-width` (defaults to 1) is the width of the stroke assuming the icon's viewBox is 1024 units tall but the
+  icon is rendered at 32px (so it's multiplied by 32).
+
+> **Aside**: the tool used to build the icon library scales up the viewBox to 1024 tall and then rounds
+> all coordinates to nearest integer on the assumption that this is plenty precise enough for icons and
+> makes everything smaller and easier to compress.
 
 ```html
-<xin-icon class="demo-2" icon="game" color="var(--brand-color)"></xin-icon>
+<xin-icon size="64" icon="game" color="var(--brand-color)"></xin-icon>
+<xin-icon size="96" icon="game" color="yellow" stroke="black"></xin-icon>
+<xin-icon size="64" icon="star"
+ color="linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)"
+></xin-icon>
+<xin-icon size="64" icon="star"
+ color="linear-gradient(345deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)"
+></xin-icon>
+<xin-icon size="64" icon="star"
+ color="linear-gradient(180deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)"
+></xin-icon>
+<xin-icon size="64" icon="star"
+ color="linear-gradient(90deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)"
+></xin-icon>
 ```
 ```css
 xin-icon.demo-2 > svg {
@@ -91,7 +113,37 @@ xin-icon.demo-2 > svg {
 }
 ```
 
+**CSS-to-SVG Gradient** support is work-in-progress and experimental (there seem to be
+…issues… with how SVG  gradients behave). The goal is to be able to use CSS gradients
+to generate SVG gradients (which are kind of a pain) on-the-fly. Use at your own risk.
+
 ## SVGs as data-urls
+
+```js
+const { elements } = xinjs
+const { icons, svg2DataUrl } = xinjsui
+
+preview.append(
+  elements.span({
+    style: {
+      display: 'inline-block',
+      width: '120px',
+      height: '24px',
+      content: '" "',
+      background: svg2DataUrl(icons.star(), 'none', '#bbb', 3)
+    }
+  }),
+  elements.span({
+    style: {
+      display: 'inline-block',
+      width: '120px',
+      height: '24px',
+      content: '" "',
+      background: svg2DataUrl(icons.star(), 'gold', 'orange', 2)
+    }
+  }),
+)
+```
 
 `svg2DataUrl(svg: SVGElement, fill?: string, stroke?: string): string` is provided as a
 utility for converting SVG elements into data-urls (e.g. for incorporation into
@@ -112,6 +164,21 @@ integrating custom icon fonts or stylesheets needed by code libraries (and an ic
 a style-sheet. Importing code is simply easier (and as a bonus, more compact and flexible, and there's
 no question as to when the stuff is available).
 
+Until I wrote this library, I had settled on icomoon.io's system for generating and maintaining
+custom icon fonts for managing icons within a project, but this makes exporting UI elements
+with icons in them fiddly, and I looked at other UI libraries and found similar issues.
+
+Even when just using this approach for projects over which I had full control, there were issues
+with syncing icons with CSS (e.g. if you want to attach an element to a pseudo-element). `icons`
+in combination with `svg2DataUrl` solves all these problems.
+
+Basically, I wanted an icon solution that "just works" and this is it.
+
+> Multi-color icons are not yet supported, but it's on the list and the colors
+> will be overridable via styling.
+
+Internally, icons are stored as javascript path data.
+
 These icons are mainly sourced from [feather](https://github.com/feathericons/feather), but
 all the icons have been processed to have integer coordinates in a `viewBox` typically scaled to 1024  &times; 1024.
 
@@ -130,7 +197,10 @@ export declare const svg2DataUrl: (svg: SVGElement, fill?: string, stroke?: stri
 export declare const icons: SVGIconMap;
 export declare class SvgIcon extends WebComponent {
     icon: string;
+    size: number;
     color: string;
+    stroke: string;
+    strokeWidth: number;
     constructor();
     render(): void;
 }
