b6f026dd199eba0bdd1af1d547303064c1dac168
Tonio Loewald <tonio@loewald.com>
2024-03-14 18:22:48 +0200

    [wip] segmented-select

---
+++ src/segmented.ts
@@ -0,0 +1,272 @@
+/*!
+# Segmented Select
+
+This is a fairly general-purpose segmented select control.
+
+```html
+<blockquote>
+Check the console to see the values being set.
+</blockquote>
+
+<div class="grid">
+<segmented-select value="yes" choices="yes, no, don't care">
+  Should we?
+</segmented-select>
+
+<segmented-select title="do you like?" choices="yes=Yes:thumbsUp, no=No:thumbsDown"></segmented-select>
+
+<segmented-select
+  style="--segment-direction: column; --segment-align-items: stretch" 
+  choices="in a relationship, single" other="it's complicated…" 
+  placeholder="oooh… please elaborate"
+  value="separated"
+>
+  Relationship Status
+</segmented-select>
+
+<segmented-select 
+  multiple 
+  style="--segment-direction: column; --segment-align-items: start" 
+  choices="star=Star:star, game=Game:game, bug=Bug:bug, camera=Camera:camera"
+  value="star,bug"
+>
+  Pick all that apply
+</segmented-select>
+</div>
+```
+```css
+.preview .grid {
+  padding: 16px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+}
+```
+```js
+function logEvent(event) {
+  const { target } = event
+  if (target.tagName === 'SEGMENTED-SELECT') {
+    console.log((target.textContent || target.title).trim(), target.value)
+  }
+}
+preview.addEventListener('change', logEvent, true)
+```
+
+## Properties
+
+- `values` is an array of values (only really useful if `multiple` is set to true)
+
+You can set `choices` programmatically to an array of `Choice` objects:
+
+    interface Choice {
+      icon?: string | SVGElement
+      value: string
+      caption: string
+    }
+
+## Attributes
+
+- `choices` is a string of comma-delimited options of the form `value=caption:icon`, 
+  where caption and icon are optional
+- `multiple` allows multiple selection
+- `name` allows you to set the name of the `<input>` elements to a specific value, it will default 
+  to the component's `instanceId`
+- `other` (default '', meaning other is not allowed) is the caption for other options, allowing 
+  the user to input their choice. It will be reset to '' if `multiple` is set.
+- `placeholder` is the placeholder displayed in the `<input>` field for **other** responses
+
+## Styling
+
+Customization via CSS-variables.
+*/
+
+import { Component as WebComponent, ElementCreator, elements, vars, varDefault } from 'xinjs'
+import { icons } from './icons'
+
+const {div, slot, label, span, input} = elements
+
+const OTHER = '_OTHER_'
+
+interface Choice {
+  icon?: string | SVGElement
+  value: string
+  caption: string
+}
+
+interface SegmentParts {
+  [key: string]: HTMLElement
+  custom: HTMLInputElement
+}
+
+class SegmentedSelect extends WebComponent {
+  choices: string | Choice[] = ''
+  other: string = ''
+  multiple = false
+  name = ''
+  placeholder = 'Please specify…'
+
+  value = ''
+
+  get values (): string[] {
+    return this.value.split(',').map(v => v.trim()).filter(v => v !== '')
+  }
+
+  content = () => [ slot(), div({part: 'options'}, input({part: 'custom', hidden: true})) ]
+
+  static styleSpec = {
+    ':host': {
+      display: 'inline-flex',
+      flexDirection: varDefault.segmentDirection('row'),
+      alignItems: varDefault.segmentAlignItems('center'),
+      gap: varDefault.segmentedOGap('8px'),
+    },
+    ':host label': {
+      display: 'inline-grid',
+      alignItems: 'center',
+      gap: varDefault.segmentedOGap('8px'),
+      gridTemplateColumns: varDefault.segmentedOptionGridColumns('0px 24px 1fr'),
+      padding: varDefault.segmentedOptionPadding('4px 12px'),
+      fontSize: varDefault.segmentedOptionFontSize('16px')
+    },
+    ':host label:has(:checked)': {
+      color: varDefault.segmentedOptionCurrentColor('#eee'),
+      background: varDefault.segmentedOptionCurrentBackground('#44a'),
+    },
+    ':host svg': {
+      fill: varDefault.segmentedOptionIconColor('currentColor'),
+    },
+    ':host label.no-icon': {
+      gap: 0,
+      gridTemplateColumns: varDefault.segmentedOptionGridColumns('0px 1fr'),
+    },
+    ':host::part(options)': {
+      display: 'flex',
+      flexDirection: varDefault.segmentDirection('row'),
+      borderRadius: varDefault.segmentedOptionsBorderRadius('8px'),
+      background: varDefault.segmentedOptionsBackground('#fff'),
+      color: varDefault.segmentedOptionColor('#222'),
+      overflow: 'hidden',
+    },
+    ':host::part(custom)': {
+      padding: varDefault.segmentedOptionPadding('4px 12px'),
+      fontSize: varDefault.segmentedOptionFontSize('16px'),
+    },
+    ':host::part(custom):not(:disabled)': {
+      color: varDefault.segmentedOptionCurrentColor('#eee'),
+      background: varDefault.segmentedOptionCurrentBackground('#44a'),
+    }
+  }
+
+  constructor() {
+    super()
+
+    this.initAttributes('direction', 'choices', 'other', 'multiple', 'name', 'placeholder')
+  }
+
+  handleChange = (event: Event) => {
+    const { target } = event
+
+    if (target instanceof HTMLInputElement) {
+      if (this.multiple) {
+        const values = new Set(this.values)
+        if (target.checked) {
+          values.add(target.value)
+        } else {
+          values.delete(target.value)
+        }
+        this.value = [...values].join(',')
+      } else {
+        if ((target.type !== 'radio' && !target.disabled) || target.checked) {
+          this.value = target.value
+        }
+      }
+    }
+  }
+
+  handleKey = (event: KeyboardEvent) => {
+    switch(event.code) {
+      case 'Space':
+        (event.target as HTMLLabelElement).click()
+        break
+    }
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback()
+    const { options } = this.parts
+
+    if (this.name === '') {
+      this.name = this.instanceId
+    }
+
+    options.addEventListener('change', this.handleChange)
+    options.addEventListener('keydown', this.handleKey)
+
+    if (this.other && this.multiple) {
+      console.warn(this, 'is set to [other] and [multiple]; [other] will be ignored')
+      this.other = ''
+    }
+  }
+
+  private get _choices(): Choice[] {
+    const options: Choice[] = Array.isArray(this.choices) ? this.choices : this.choices.split(',').filter(c => c.trim() !== '').map(c => {
+      const [value, remains] = c.split('=').map(v => v.trim())
+      const [caption, iconName] = (remains || value).split(':').map(v => v.trim())
+
+      const icon = iconName ? icons[iconName]() : ''
+      const choice = { value, icon, caption } 
+      return choice
+    })
+
+    if (this.other && !this.multiple) {
+      const [caption, icon] = this.other.split(':')
+      options.push({
+        value: OTHER,
+        caption,
+        icon
+      })
+    }
+
+    return options
+  }
+
+  get isOtherValue(): boolean {
+    return Boolean(this.value === OTHER || (this.value && !this._choices.find(choice => choice.value === this.value)))
+  }
+
+  render() {
+    super.render()
+
+    const { options, custom } = this.parts as SegmentParts
+    options.textContent = ''
+    const type = this.multiple ? 'checkbox' : 'radio'
+    const { values, isOtherValue } = this
+    options.append(...this._choices.map(choice => {
+      return label(
+        {tabindex: 0},
+        input({
+          type, 
+          name: this.name, 
+          value: choice.value, 
+          checked: values.includes(choice.value) || (choice.value === OTHER && isOtherValue),
+          tabIndex: -1
+        }),
+        choice.icon || { class: 'no-icon' },
+        span(choice.caption)
+      )
+    }))
+    if (this.other && !this.multiple && isOtherValue) {
+      if (this.value !== OTHER && custom.value !== this.value) {
+        custom.value = this.value
+      } else if (this.value === OTHER && custom.value) {
+        this.value = custom.value
+      }
+      custom.hidden = false
+      custom.placeholder = this.placeholder
+      options.append(custom)
+      custom.focus()
+    }
+  }
+}
+
+export const segmentedSelect = SegmentedSelect.elementCreator({tag: 'segmented-select'}) as ElementCreator<SegmentedSelect>
