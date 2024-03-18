197abe2d216cc111d9b5c048a0f95ade93caad08
Tonio Loewald <tonio@loewald.com>
2024-03-18 23:07:53 +0200

    Update css-var-editor.ts
    [fix] leveraging changeCallback to simplify update
    [fix] removed spam
    [fix] added support for non shadowRoot targets

--- demo/css-var-editor.ts
+++ demo/css-var-editor.ts
@@ -20,7 +20,7 @@ class XinCssVarEditor extends WebComponent {
 
   content = () => [
     h2({ part: 'title' }, 'CSS variables'),
-    xinForm({ part: 'variables' }),
+    xinForm({ part: 'variables', changeCallback: this.update }),
   ]
 
   loadVars = () => {
@@ -35,40 +35,36 @@ class XinCssVarEditor extends WebComponent {
         setTimeout(this.loadVars, 250)
         return
       }
-      if (element.shadowRoot) {
-        const styleNode = element.shadowRoot.querySelector('style')
-        const computedStyle = getComputedStyle(element)
-        if (styleNode && styleNode.textContent) {
-          const cssVars = [
-            ...new Set([...(styleNode.textContent!.match(/--[\w-]+/g) || [])]),
-          ]
+      const styleNode = element.shadowRoot ? element.shadowRoot.querySelector('style') : document.querySelector(`style#${this.elementSelector}-component`)
+      const computedStyle = getComputedStyle(element)
+      if (styleNode && styleNode.textContent) {
+        const cssVars = [
+          ...new Set([...(styleNode.textContent!.match(/--[\w-]+/g) || [])]),
+        ]
 
-          for (const cssVar of cssVars) {
-            let value = computedStyle.getPropertyValue(cssVar)
-            const type = value.match(/^(#|rgb|hsl)[\d()a-fA-F]+$/)
-              ? 'color'
-              : 'string'
-            if (type === 'color') {
-              value = Color.fromCss(value).html
-            }
-            variables.append(
-              xinField(code(cssVar), { key: cssVar, value, type })
-            )
+        for (const cssVar of cssVars) {
+          let value = computedStyle.getPropertyValue(cssVar)
+          const type = value.match(/^(#|rgb|hsl)[\d()a-fA-F]+$/)
+            ? 'color'
+            : 'string'
+          if (type === 'color') {
+            value = Color.fromCss(value).html
           }
+          variables.append(
+            xinField(code(cssVar), { key: cssVar, value, type })
+          )
         }
       }
-    }
+    } 
   }
 
-  update = (event: Event) => {
-    console.log(event)
+  update = () => {
     const selector = this.targetSelector || this.elementSelector
     if (selector) {
       const target = document.querySelector(selector) as HTMLElement | null
       if (!target) {
         return
       }
-      console.log(target)
       const { value } = this.parts.variables as XinForm
       for (const key of Object.keys(value)) {
         target.style.setProperty(key, value[key])
@@ -81,7 +77,7 @@ class XinCssVarEditor extends WebComponent {
 
     this.loadVars()
 
-    this.parts.variables.addEventListener('change', this.update, true)
+    this.parts.variables.addEventListener('change', this.update)
   }
 }
 
